a3d772c9191991674f13abf2ad07221dbfc6e629
Siyuan Cao <seeran@outlook.com>
2019-08-14 22:55:12 +0800

    feat: add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TARGET="./build/"
+
+rm -rf $TARGET
+git clone git@github.com:SR2k/sr2k.github.io $TARGET
+
+yarn build
+
+cp ../CNAME ./
+echo '# SR2k.top' >> README.md
+echo '' >> README.md
+echo 'Switch to `v2/react` branch to learn more' >> README.md
+git add -A
+git commit -m 'deploy'
+git push origin master
