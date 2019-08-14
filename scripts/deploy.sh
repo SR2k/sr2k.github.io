7e42a77e6c0686c2069c42be996da80b98ecffa6
Siyuan Cao <seeran@outlook.com>
2019-08-14 22:57:00 +0800

    fix: deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,10 +7,13 @@ git clone git@github.com:SR2k/sr2k.github.io $TARGET
 
 yarn build
 
+cd $TARGET
+
 cp ../CNAME ./
 echo '# SR2k.top' >> README.md
 echo '' >> README.md
 echo 'Switch to `v2/react` branch to learn more' >> README.md
+
 git add -A
 git commit -m 'deploy'
 git push origin master
